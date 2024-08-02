fe0b08e82b892e63f11753ae67d02857f5c858a3
ZZy979 <979481894@qq.com>
2024-08-03 01:06:46 +0800

    corejava.py支持命令行参数，增加run命令；tests.json移至根目录

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
 * 源代码：<https://horstmann.com/corejava/corejava.zip>
 * 笔记：<https://zzy979.github.io/posts/java-note-index/>
 
+# 依赖
+* JDK 17
+* Python 3
+
 # 运行方式
-在命令行中编译和运行。例如：
+方式一：在命令行中编译和运行。例如：
 
 ```shell
 cd v1ch02/Welcome
@@ -13,11 +17,23 @@ javac Welcome.java
 java Welcome
 ```
 
+方式二：使用工具脚本corejava.py。用法：
+
+```shell
+python corejava.py run {chapter}/{target}
+```
+
+其中，chapter为章节名称（例如v1ch02），target为示例程序名称，格式为`[subdir/][package.]classname`（例如Welcome/Welcome）。例如：
+
+```shell
+python corejava.py run v1ch02/Welcome/Welcome
+```
+
 # 单元测试
 在项目根目录下执行：
 
 ```shell
-python corejava.py
+python corejava.py test
 ```
 
 # 代码目录 卷I
--- corejava.py
+++ corejava.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import pathlib
 import shlex
@@ -13,8 +14,8 @@ OUT_DIR = ROOT_DIR / 'out'
 class Example:
     """书中的一个示例程序。包含以下属性：
 
-    - chapter_dir：章节目录，例如ROOT_DIR/v1ch02
     - chapter：章节名称，例如v1ch02
+    - chapter_dir：章节目录，例如ROOT_DIR/v1ch02
     - target：示例程序名称，格式为[subdir/][package.]classname，例如Foo/Bar/com.example.foo.Foo
     - sub_dir：子目录（可能为空），例如Foo/Bar
     - full_class_name：完整类名，例如com.example.foo.Foo
@@ -27,9 +28,9 @@ class Example:
     - class_dir：类文件输出目录，例如OUT_DIR/v1ch02/Foo/Bar
     """
 
-    def __init__(self, chapter_dir, target):
-        self.chapter_dir = chapter_dir
-        self.chapter = chapter_dir.name
+    def __init__(self, chapter, target):
+        self.chapter = chapter
+        self.chapter_dir = ROOT_DIR / chapter
 
         self.target = target
         target_p = pathlib.Path(target)
@@ -38,13 +39,13 @@ class Example:
         self.package_name, self.class_name = self.parse_full_class_name()
 
         self.package_dir = pathlib.Path(self.package_name.replace('.', '/'))
-        self.src_dir = chapter_dir / self.sub_dir
+        self.src_dir = self.chapter_dir / self.sub_dir
         self.src_file = self.src_dir / self.package_dir / (self.class_name + '.java')
-        self.testdata_dir = chapter_dir / 'testdata'
+        self.testdata_dir = self.chapter_dir / 'testdata'
         self.class_dir = OUT_DIR / self.chapter / self.sub_dir
 
     def __str__(self):
-        return self.target
+        return f'{self.chapter}/{self.target}'
 
     def parse_full_class_name(self):
         if '.' in self.full_class_name:
@@ -52,20 +53,25 @@ class Example:
         else:
             return '', self.full_class_name
 
-    def compile(self):
+    def build(self):
+        """编译示例程序。"""
         cmd = ['javac', '-d', self.class_dir, self.src_file]
         subprocess.run(cmd, cwd=self.src_dir, check=True)
 
-    def run(self, compile_first=True, args='', input_file=None):
-        """运行示例程序。
+    def run(self, args=''):
+        """运行示例程序。"""
+        self.build()
+        cmd = ['java', self.full_class_name, *shlex.split(args)]
+        subprocess.run(cmd, cwd=self.class_dir)
 
-        :param compile_first: bool 是否先执行编译命令
+    def test(self, args='', input_file=None):
+        """测试示例程序。
+
         :param args: str 命令行参数，空格分隔
         :param input_file: str 输入文件名
         :return: subprocess.CompletedProcess对象
         """
-        if compile_first:
-            self.compile()
+        self.build()
 
         if input_file:
             stdin = open(input_file, encoding='utf-8')
@@ -82,27 +88,31 @@ class Example:
         return result
 
     def clean(self):
+        """清理编译输出。"""
         shutil.rmtree(self.class_dir)
 
 
 class TestCase:
 
-    def __init__(self, chapter_dir, config):
+    def __init__(self, chapter, config):
         """一个示例程序的测试用例。
 
-        :param chapter_dir: pathlib.Path 章节目录
+        :param chapter: str 章节名称
         :param config: dict 测试用例配置，包含以下键值对：
             "target": 示例程序名称，格式同Example.target
             "args": 命令行参数（可选）
             "input_file": 标准输入文件（可选）
             "output_file": 用于比较标准输出的文件
         """
-        self.example = Example(chapter_dir, config['target'])
+        self.example = Example(chapter, config['target'])
         self.config = config
         self.args = self.config.get('args', '')
         self.input_file = self.example.testdata_dir / config['input_file'] \
             if 'input_file' in config else None
         self.output_file = self.example.testdata_dir / config['output_file']
+
+    def __str__(self):
+        return str(self.example)
 
     def run(self):
         """运行示例程序，并将标准输出与指定的文件比较。
@@ -111,28 +121,60 @@ class TestCase:
         """
         with open(self.output_file, encoding='utf-8') as f:
             expected_output = f.read()
-        result = self.example.run(args=self.args, input_file=self.input_file)
+        result = self.example.test(args=self.args, input_file=self.input_file)
         return expected_output == result.stdout
 
 
-def main():
+def load_tests():
+    """加载所有的测试用例。"""
+    with open(ROOT_DIR / 'tests.json', encoding='utf-8') as f:
+        tests = json.load(f)
+    for chapter, configs in tests.items():
+        for config in configs:
+            yield TestCase(chapter, config)
+
+
+def run(args):
+    """运行示例程序。"""
+    chapter, target = args.target.split('/', 1)
+    Example(chapter, target).run()
+
+
+def test(args):
+    """运行测试。"""
     run = failed = 0
-    for chapter_dir in ROOT_DIR.glob('v*'):
-        test_file = chapter_dir / 'testdata/tests.json'
-        if test_file.exists():
-            with open(test_file, encoding='utf-8') as f:
-                configs = json.load(f)
-            for config in configs:
-                print('Testing {}...'.format(config['target']), end='')
-                run += 1
-                test_case = TestCase(chapter_dir, config)
-                if test_case.run():
-                    print('OK')
-                else:
-                    print('failed')
-                    failed += 1
+    for test_case in load_tests():
+        print(f'Testing {test_case}...', end='')
+        run += 1
+        if test_case.run():
+            print('OK')
+        else:
+            print('failed')
+            # TODO diff
+            failed += 1
     print(f'Run {run} tests, {failed} failed')
     sys.exit(failed)
+
+
+def create_arg_parser():
+    parser = argparse.ArgumentParser(description='《Java核心技术》书中示例程序运行和测试工具')
+    parser.set_defaults(func=lambda args: parser.print_help())
+    sub_parsers = parser.add_subparsers(help='子命令')
+
+    run_parser = sub_parsers.add_parser('run', help='运行示例程序')
+    run_parser.add_argument('target', help='示例程序名称（例如v1ch02/Welcome/Welcome）')
+    run_parser.set_defaults(func=run)
+
+    test_parser = sub_parsers.add_parser('test', help='运行测试')
+    test_parser.set_defaults(func=test)
+
+    return parser
+
+
+def main():
+    parser = create_arg_parser()
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
